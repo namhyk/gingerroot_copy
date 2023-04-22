4185af2678f8b1341260202c461c0d3b10cc2a58
Namhyun <nhkz1005@gmail.com>
2023-04-22 16:40:37 +1200

    Update navbar
    Show and hide navbar on click

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ mobileNavLabel.addEventListener('click',
 function() {
   console.log('clicked')
   mobileNavLabel.classList.toggle('active')
+  if (mobileNavLabel.classList.contains('active')){
+    document.getElementById('nav-el').style.display = 'block'
+  } else {
+    document.getElementById('nav-el').style.display = 'none'
+  }
 })
+
